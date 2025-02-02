764f46ddc17899d8c6a2b603f5881b42acdce267
DrunkenCloud <drunkencloud99@gmail.com>
2025-02-02 07:35:27 +0530

    Added resume and image changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
 
 class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -7,7 +8,14 @@ class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Expires', '0')
         super().end_headers()
 
+    def do_GET(self):
+        if self.path == '/resume':
+            self.path = '/static/resume.pdf'
+        return super().do_GET()
+
 if __name__ == '__main__':
+    os.chdir('.')
     server_address = ('', 3000)
     httpd = HTTPServer(server_address, NoCacheHTTPRequestHandler)
+    print("Serving on port 3000...")
     httpd.serve_forever()
